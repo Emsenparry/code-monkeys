f5715d91d7c1a828257132648af250104cf0cdd9
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-06-20 14:39:03 +0200

    controller and router
    Doesn't work yet

---
+++ controllers/product.controller.js
@@ -0,0 +1,19 @@
+import db from "../Config/db.config.js";
+
+class productController {
+  constructor() {
+    console.log("Class productController instantiated");
+  }
+
+  list = (req, res) => {};
+
+  details = (req, res) => {};
+
+  create = (req, res) => {};
+
+  update = (req, res) => {};
+
+  // delete
+}
+
+export default productController;
